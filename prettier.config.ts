98b1462a4bb5dba82f5d057de66c850b28462087
nits68 <nits.laszlo@jedlik.eu>
2025-11-28 22:58:53 +0100

    correct formatting error, replace prettier config file for solution

---
+++ prettier.config.ts
@@ -0,0 +1,30 @@
+// prettier.config.ts
+import { type Config } from "prettier";
+// Trükk: A pluginok útvonalának feloldásához a "module" csomag kell
+import { createRequire } from "module";
+
+const require = createRequire(import.meta.url);
+
+const config: Config = {
+  singleQuote: false,
+  semi: true,
+  trailingComma: "all",
+  tabWidth: 2,
+  printWidth: 100,
+  plugins: [
+    // Itt is kötelező a feloldás, különben a VS Code nem találja meg őket
+    require.resolve("@trivago/prettier-plugin-sort-imports"),
+    require.resolve("prettier-plugin-tailwindcss"),
+  ],
+  importOrder: [
+    "<THIRD_PARTY_MODULES>",
+    "^@/(.*)$",
+    "^[./]",
+  ],
+  importOrderSeparation: false,
+  importOrderSortSpecifiers: true,
+  tailwindFunctions: ["clsx"],
+  tailwindStylesheet: "./app/globals.css",
+};
+
+export default config;
