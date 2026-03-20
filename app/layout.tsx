5fd1768c7358f8b7c667877edddd55fc9f84d5a7
nits68 <nits.laszlo@jedlik.eu>
2026-03-20 10:26:17 +0100

    remove Google fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import { Toaster } from "react-hot-toast";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "next-frontend-starter",
@@ -24,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`} lang="en">
+    <html className={"h-full antialiased"} lang="en">
       <body className="flex min-h-full flex-col">
         <Toaster position="bottom-right" toastOptions={{ duration: 5000 }} />
         {children}
